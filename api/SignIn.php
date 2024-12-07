f30dc1b879d7d7d02deb855e7831357664ebc6f3
kingkenrin <huypqkgvn1973@gmail.com>
2024-12-07 10:05:03 +0700

    handle cors

--- api/SignIn.php
+++ api/SignIn.php
@@ -2,11 +2,21 @@
 require "../bootstrap.php";
 use Src\Controller\SignInController;
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+// header("Access-Control-Allow-Origin: *");
+// header("Content-Type: application/json; charset=UTF-8");
+// header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+// header("Access-Control-Max-Age: 3600");
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Max-Age: 3600");
+    http_response_code(204); // Không trả về nội dung nào
+    exit(0);
+}
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
