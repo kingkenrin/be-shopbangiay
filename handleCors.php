a75cbe86a425a015275cf889a663071c734b8b67
kingkenrin <huypqkgvn1973@gmail.com>
2024-12-10 13:42:33 +0700

    update cors

--- handleCors.php
+++ handleCors.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
