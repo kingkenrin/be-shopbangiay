84ed1d050331d4aaad9568f8f8da14c0c9001dc6
kingkenrin <huypqkgvn1973@gmail.com>
2024-12-10 14:07:20 +0700

    update

--- handleCors.php
+++ handleCors.php
@@ -6,11 +6,6 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Origin: *");
-    header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-    header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     header('HTTP/1.1 200 OK');
     exit();
 }
